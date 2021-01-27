eb74ab62594b70546b26b6a502bbfc37cf1c1dcd
Beny <b.ntambwe97@gmail.com>
2021-01-27 09:28:41 +0100

    fin exo

---
+++ main.js
@@ -0,0 +1,14 @@
+// 1
+
+let para1 = document.getElementsByTagName('p')[1]
+console.log(para1.innerText);
+
+// 2
+
+let para2 = document.getElementsByTagName('p')[2]
+console.log(para2.innerText);
+
+// 3
+
+let para3 = document.getElementsByTagName('p')[4]
+console.log(para3);
