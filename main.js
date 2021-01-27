ee8b2980c355a4f24138de504c95e17561c8c310
Beny <b.ntambwe97@gmail.com>
2021-01-27 09:59:19 +0100

    +correction

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // 1
 
-let para1 = document.getElementsByTagName('p')[1]
-console.log(para1.innerText);
+let para1 = document.getElementsByTagName('p')
+console.log(para1[0].innerText);
 
 // 2
 
@@ -12,3 +12,23 @@ console.log(para2.innerText);
 
 let para3 = document.getElementsByTagName('p')[4]
 console.log(para3);
+
+
+
+// CORRECTION
+
+
+//1 par la class
+let classTartine = document.getElementsByClassName('tartine')
+console.log(classTartine[0].innerText);
+
+//2 par l'id
+let idToast = document.getElementById('toast')
+console.log(idToast.innerText);
+
+//3
+console.log(classTartine[1]);
+
+
+// par la balise
+let titre = document.getElementsByTagName('h1')
